17346908153d4416df2176263275904ce93c29c6
GitHub Action <action@github.com>
2021-04-22 20:20:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/eaghayi/clusterusages/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/eaghayi/clusterusages/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.eaghayi.clusterusages.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.eaghayi.clusterusages.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/eaghayi/clusterusages/services/MyProjectService.kt
+++ src/main/kotlin/com/github/eaghayi/clusterusages/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.eaghayi.clusterusages.services
 
+import com.github.eaghayi.clusterusages.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
