f7d4026ff748351fa0ba6c6fef43bc144519a608
Emad Aghayi <emad.aghayi@gmail.com>
2021-05-07 14:34:50 -0400

    allow same usage to be grouped.

--- src/main/java/backend/UsageAggregator.java
+++ src/main/java/backend/UsageAggregator.java
@@ -10,21 +10,21 @@ import java.util.*;
 
 public class UsageAggregator {
 
-    // If we set the similarity threshold to 0.15, usages that are less than 85% similar are unlikely to be assigned to the same group.
+    // If we set the similarity threshold to 0.25, usages that are less than 75% similar are unlikely to be assigned to the same group.
     // If we set the SIMILIAR_THRESHOLD =0 we can say usages less than 100% similar usages (identical usages) are placed to different groups.
     private final static double SIMILIAR_THRESHOLD = 0.15; // the threshold is inverted and means allowed difference between usages within one group.
     private List<UsageGroupAst> astSimilarityList = new LinkedList<>();
-    private Set<String> codeBlockSet = new HashSet<>();
+//    private Set<String> codeBlockSet = new HashSet<>();
 
     public synchronized UsageGroup getAggregateUsage(Usage usage) {
         UsageInfo usageInfo = ((UsageInfo2UsageAdapter) usage).getUsageInfo();
         PsiElement codeBlockPsiElement = getFirstCodeBlockParent(usageInfo.getElement());
         if (codeBlockPsiElement == null) return null;
-        if (codeBlockSet.contains(codeBlockPsiElement.getText())) {
-            return null;
-        } else {
-            codeBlockSet.add(codeBlockPsiElement.getText());
-        }
+//        if (codeBlockSet.contains(codeBlockPsiElement.getText())) {
+//            return null;
+//        } else {
+//            codeBlockSet.add(codeBlockPsiElement.getText());
+//        }
         CodeBlock codeBlock = new CodeBlock(codeBlockPsiElement);
         return cluster(codeBlock);
     }
@@ -40,11 +40,11 @@ public class UsageAggregator {
                         (int) (1000 * (a.getMinimumSimilarityTo(codeBlock) - b.getMinimumSimilarityTo(codeBlock))));
 
         if (mostSimilarCodeBlock.isPresent()) {
-            // calculate similarity score to the most similar usage in all usages
+             // calculate similarity score to the most similar usage in all usages
             highestSimilarityRating = mostSimilarCodeBlock.get().getMinimumSimilarityTo(codeBlock);
         }
         // Check if returning exact match because classic find usages is weird.
-        if (highestSimilarityRating >= 1.0) {
+        if (highestSimilarityRating > 1.0) {
             throw new RuntimeException("This should never happen!");
         }
         if (highestSimilarityRating > SIMILIAR_THRESHOLD) {
