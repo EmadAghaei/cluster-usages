baa59467af3efad9b35a643676ee65de872eb803
Emad Aghayi <emad.aghayi@gmail.com>
2021-06-30 21:27:05 +0430

    fix numbering presentation issue

--- src/main/java/backend/UniqueUsageGroup.java
+++ src/main/java/backend/UniqueUsageGroup.java
@@ -1,17 +1,20 @@
 package backend;
 
 import com.intellij.openapi.externalSystem.service.execution.NotSupportedException;
+import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.vcs.FileStatus;
 import com.intellij.usages.UsageGroup;
 import com.intellij.usages.UsageView;
+import com.intellij.usages.impl.rules.UsageGroupBase;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class UniqueUsageGroup implements UsageGroup {
     String usageDisplayed;
-    int count = 1;
+    AtomicInteger count =  new AtomicInteger(0);
 
     public UniqueUsageGroup() {
         super();
@@ -23,7 +26,8 @@ public class UniqueUsageGroup implements UsageGroup {
     }
 
     void incrementUsageCount() {
-        count++;
+
+        count.getAndIncrement();
     }
 
 
@@ -73,9 +77,22 @@ public class UniqueUsageGroup implements UsageGroup {
 
     @Override
     public int compareTo(@NotNull UsageGroup o) {
+//        if (!(o instanceof UniqueUsageGroup)) {
+//            return -1;
+//        }
+        return getText(null).compareToIgnoreCase(o.getText(null));
+//        return this.count.get() - ((UniqueUsageGroup) o).count.get();
+//        int order = this.
+//        if (order != 0) {
+//            return order;
+//        }
+//        return getText(null).compareToIgnoreCase(o.getText(null));
+    }
+    /* @Override
+    public int compareTo(@NotNull UsageGroup o) {
         if (o instanceof UniqueUsageGroup) {
-            return this.count - ((UniqueUsageGroup) o).count;
+            return this.count.get() - ((UniqueUsageGroup) o).count.get();
         }
         return 0;
-    }
+    }*/
 }
